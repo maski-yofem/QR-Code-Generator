54d5aef8cf97d9c2f38b43abb2d3c6ccad55ca93
Maski <maxlucasaleixo@gmail.com>
2026-04-10 16:48:37 -0300

    Configured the path for github page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Define que o Next vai gerar arquivos estáticos
+  basePath: '/QR-Code-Generator', // Nome exato do seu repositório no GitHub
+  images: {
+    unoptimized: true, // Necessário pois o GitHub Pages não suporta a otimização de imagem padrão do Next
+  },
 };
 
 export default nextConfig;
